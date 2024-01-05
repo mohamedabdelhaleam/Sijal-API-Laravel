aa417b7c5f717c372e6260cb665ba8be3b7833ca
Mohamed abdelhaleam <mohamedabdelhaleam13@gmail.com>
2024-01-05 21:51:03 +0200

    create Order Model

---
+++ app/Models/Order.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Order extends Model
+{
+    use HasFactory;
+    protected $table = "orders";
+    protected $fillable = ['total_amount', 'status', 'order_date', 'user_id'];
+    protected $hidden = [];
+    public $timestamps = true;
+}
