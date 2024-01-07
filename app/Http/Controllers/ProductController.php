9c95a58c4e0bb685e34c8ce57d891a8e1c9832ed
Mohamed abdelhaleam <mohamedabdelhaleam13@gmail.com>
2024-01-07 20:14:20 +0200

    Add Functions to Product Controller

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -2,9 +2,102 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\ProductRequest;
+use App\Models\Product;
 use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
-    //
+    public function getAllProduct()
+    {
+        $product = Product::with('Reviews')->get();
+        if (!$product) {
+            return response()->json([
+                'status' => "fail",
+                'data' => [
+                    'product' => 'No Product'
+                ]
+            ], 204);
+        }
+        return response()->json([
+            'status' => "success",
+            'data' => [
+                'products' => $product
+            ]
+        ], 200);
+    }
+    public function getSingleProduct($productId)
+    {
+        $product = Product::find($productId);
+        if (!$product) {
+            return response()->json([
+                'status' => "fail",
+                'message' => "Not Found",
+                'data' => null
+            ], 404);
+        }
+        return response()->json([
+            'status' => "success",
+            'data' => [
+                'product' => $product
+            ]
+        ], 200);
+    }
+    public function createProduct(ProductRequest $request)
+    {
+        $product = Product::create([
+            'name' => $request->name,
+            'description' => $request->description,
+        ]);
+        if (!$product) {
+            return response()->json([
+                'status' => "fail",
+                'message' => "Product Created Faild",
+                'data' => null
+            ], 404);
+        }
+        return response()->json([
+            'status' => "success",
+            'message' => "Product Created Successfully",
+            'data' => [
+                'product' => $product
+            ]
+        ], 201);
+    }
+    public function updateProduct(ProductRequest $request, $productId)
+    {
+        $product = Product::find($productId);
+        if (!$product) {
+            return response()->json([
+                'status' => "fail",
+                'message' => "Not Found",
+                'data' => null
+            ], 404);
+        }
+        $product->update($request->all());
+        return response()->json([
+            'status' => "success",
+            'message' => "Product Updated Successfully",
+            'data' => [
+                'product' => $product
+            ]
+        ], 200);
+    }
+    public function deleteProduct($productId)
+    {
+        $product = Product::find($productId);
+        if (!$product) {
+            return response()->json([
+                'status' => "fail",
+                'message' => "Not Found",
+                'data' => null
+            ], 404);
+        }
+        $product->delete();
+        return response()->json([
+            'status' => "success",
+            'message' => "Product Deleted Successfully",
+            'data' => null
+        ], 200);
+    }
 }
