834889231b675f4feefc5bf4f28ea60498397632
Mohamed abdelhaleam <mohamedabdelhaleam13@gmail.com>
2024-01-09 17:14:56 +0200

    Add Relations to Cart items

--- app/Models/CartItem.php
+++ app/Models/CartItem.php
@@ -10,6 +10,16 @@ class CartItem extends Model
     use HasFactory;
     protected $table = "cart_items";
     protected $fillable = ['quantity', 'cart_id', 'product_id'];
-    protected $hidden = [];
+    protected $hidden = ['pivot', 'product_id'];
     public $timestamps = false;
+
+
+    public function Cart()
+    {
+        return $this->belongsTo(Cart::class, 'cart_id', 'id');
+    }
+    public function Products()
+    {
+        return $this->belongsToMany(Product::class, 'cart_product', 'cart_items_id', 'product_id');
+    }
 }
