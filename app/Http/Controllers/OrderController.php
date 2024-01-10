3d571e7c95c6ff92b55dac9121dda34e81230548
Mohamed abdelhaleam <mohamedabdelhaleam13@gmail.com>
2024-01-10 22:42:35 +0200

    Create Order Controller

---
+++ app/Http/Controllers/OrderController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class OrderController extends Controller
+{
+    public function getUserOrder()
+    {
+        $order = User::with('Orders')->find(auth()->user());
+        if (!$order) {
+            return response()->json([
+                'status' => "fail",
+                'message' => "Not Found",
+                'data' => null
+            ], 404);
+        }
+        return response()->json([
+            'status' => "success",
+            'data' => [
+                'user' => $order
+            ]
+        ], 200);
+    }
+}
