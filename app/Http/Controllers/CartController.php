46659e3ba461cbdd947a9c1070fb35e1bc5c9ea5
Mohamed abdelhaleam <mohamedabdelhaleam13@gmail.com>
2024-01-10 16:26:50 +0200

    Edit Response in Cart Controller

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -3,22 +3,24 @@
 namespace App\Http\Controllers;
 
 use App\Models\Cart;
+use Illuminate\Support\Facades\Response;
 use Illuminate\Http\Request;
 
 class CartController extends Controller
 {
-    public function getUserCart($cartId)
+    public function getOneCart($cartId)
     {
-        $cart = Cart::with('CartItems')->find($cartId);
+        $cart = Cart::with('cartItems.Products')->find($cartId);
         if (!$cart) {
             return response()->json([
                 'status' => "fail",
                 'message' => "Not Found",
                 'data' => null
-            ], 404);
+            ], 201);
         }
         return response()->json([
             'status' => "success",
+            'message' => null,
             'data' => [
                 'cart' => $cart
             ]
