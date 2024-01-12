0c5ee84f89a48ffec18695ea71c2a081db2c0429
Mohamed abdelhaleam <mohamedabdelhaleam13@gmail.com>
2024-01-12 20:47:23 +0200

    create function to create order

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Order;
 use App\Models\User;
+use DateTime;
 use Illuminate\Http\Request;
 
 class OrderController extends Controller
@@ -23,5 +25,30 @@ class OrderController extends Controller
                 'user' => $order
             ]
         ], 200);
+    }
+    public function createOrder()
+    {
+        $user = auth()->user();
+        if ($user) {
+            $order = Order::create([
+                'total_amount' => 200,
+                'status' => "wait",
+                'user_id' => $user->id,
+            ]);
+            if (!$order) {
+                return response()->json([
+                    'status' => "fail",
+                    'message' => "Not Found",
+                    'data' => null
+                ], 404);
+            }
+            return response()->json([
+                'status' => "success",
+                'data' => [
+                    'order' => $order
+                ]
+            ], 201);
+        }
+        return response()->json(['error' => 'Unauthorized'], 401);
     }
 }
