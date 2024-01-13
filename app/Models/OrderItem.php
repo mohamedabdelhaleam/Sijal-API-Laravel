24226b2bc6d6be596ac19c490328fc25b1a4afaf
Mohamed abdelhaleam <mohamedabdelhaleam13@gmail.com>
2024-01-13 21:18:20 +0200

    Delete Sub total

--- app/Models/OrderItem.php
+++ app/Models/OrderItem.php
@@ -9,7 +9,7 @@ class OrderItem extends Model
 {
     use HasFactory;
     protected $table = "order_items";
-    protected $fillable = ['quantity', 'sub_total', 'order_id', 'product_id'];
+    protected $fillable = ['quantity', 'order_id', 'product_id'];
     protected $hidden = ['pivot'];
     public $timestamps = false;
 
