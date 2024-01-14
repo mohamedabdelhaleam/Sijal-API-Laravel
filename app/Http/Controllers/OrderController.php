7bc7cc95904cc9bbbaae9ab53449584fe339d214
Mohamed abdelhaleam <mohamedabdelhaleam13@gmail.com>
2024-01-14 18:47:35 +0200

    Edit Some Functions

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -2,16 +2,38 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Cart;
 use App\Models\Order;
-use App\Models\User;
-use DateTime;
-use Illuminate\Http\Request;
+use App\Models\OrderItem;
 
 class OrderController extends Controller
 {
-    public function getUserOrder()
+    public function __construct()
     {
-        $order = User::with('Orders.OrderItems')->find(auth()->user());
+        $this->middleware('auth:api');
+    }
+    public function getOrder()
+    {
+        $user = auth()->user();
+        $order = Order::where("user_id", $user->id)->with('OrderItems')->get();
+        if (!$order) {
+            return response()->json([
+                'status' => "fail",
+                'message' => "Not Found",
+                'data' => null
+            ], 404);
+        }
+        return response()->json([
+            'status' => "success",
+            'data' => [
+                'user' => $order
+            ]
+        ], 200);
+    }
+    public function getOrdersWithProducts()
+    {
+        $user = auth()->user();
+        $order = Order::where("user_id", $user->id)->with('OrderItems.Products')->get();
         if (!$order) {
             return response()->json([
                 'status' => "fail",
@@ -29,26 +51,26 @@ class OrderController extends Controller
     public function createOrder()
     {
         $user = auth()->user();
-        if ($user) {
-            $order = Order::create([
-                'total_amount' => 200,
-                'status' => "wait",
-                'user_id' => $user->id,
-            ]);
-            if (!$order) {
-                return response()->json([
-                    'status' => "fail",
-                    'message' => "Not Found",
-                    'data' => null
-                ], 404);
-            }
+        $order = Order::create([
+            'total_amount' => 200,
+            'status' => "wait",
+            'user_id' => $user->id,
+        ]);
+        if (!$order) {
             return response()->json([
-                'status' => "success",
-                'data' => [
-                    'order' => $order
-                ]
-            ], 201);
+                'status' => "fail",
+                'message' => "Can not Create Order",
+                'data' => null
+            ], 404);
+        } else {
+            $cartItems = Cart::where('user_id', $user->id)->with("CartItems")->get();
+            foreach ($cartItems as $cartItem) {
+                $orderItems = OrderItem::with('Products')->create([
+                    'quantity' => 10,
+                    'order_id' => $order->id,
+                    'product_id' => 10,
+                ]);
+            }
         }
-        return response()->json(['error' => 'Unauthorized'], 401);
     }
 }
