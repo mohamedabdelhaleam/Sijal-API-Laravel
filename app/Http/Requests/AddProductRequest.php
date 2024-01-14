90df2afbf45751bfe9fe30ee2f93dab919c997b0
Mohamed abdelhaleam <mohamedabdelhaleam13@gmail.com>
2024-01-14 23:20:37 +0200

    Add Rules for Product id

---
+++ app/Http/Requests/AddProductRequest.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class AddProductRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'product_id' => 'required |unique:cart_items'
+        ];
+    }
+    public function messages()
+    {
+        return [
+            'product_id.unique' => "This Product Added Before",
+        ];
+    }
+}
