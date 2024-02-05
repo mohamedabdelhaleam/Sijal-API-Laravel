495e9247af3c322fac16b048026ffc9afebb00c2
Mohamed abdelhaleam <mohamedabdelhaleam13@gmail.com>
2024-02-05 21:21:16 +0200

    Add Some Function to user Controller

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Cart;
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class UserController extends Controller
 {
@@ -23,6 +24,42 @@ class UserController extends Controller
             'data' => [
                 'user' => $user
             ]
+        ], 200);
+    }
+    public function update(Request $request)
+    {
+        $userId = Auth::user()->id;
+        $user = User::find($userId);
+        if (!$user) {
+            return response()->json([
+                'status' => "fail",
+                'message' => "Not Found",
+                'data' => null
+            ], 404);
+        }
+        $user->update($request->all());
+        return response()->json([
+            'status' => "success",
+            'data' => [
+                'user' => $user
+            ]
+        ], 200);
+    }
+    public function delete()
+    {
+        $userId = Auth::user()->id;
+        $user = User::find($userId);
+        if (!$user) {
+            return response()->json([
+                'status' => "fail",
+                'message' => "Not Found",
+                'data' => null
+            ], 404);
+        }
+        $user->delete();
+        return response()->json([
+            'status' => "success",
+            'message' => "Deleted Successfully"
         ], 200);
     }
     public function getUserCart()
